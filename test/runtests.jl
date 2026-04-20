ed44772464029638552f68509ab53ac45e646c03
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-04-20 19:31:31 +0900

    fix: add windows bootstrap symbol lookup fallback

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using ParallelTestRunner
 using Cxx
 
 testsuite = Dict(
+    "core/bootstrap_portability" => :(include(joinpath(@__DIR__, "core", "bootstrap_portability.jl"))),
     "core/llvm_sources_paths" => :(include(joinpath(@__DIR__, "core", "llvm_sources_paths.jl"))),
     "core/smoke_load" => :(include(joinpath(@__DIR__, "core", "smoke_load.jl"))),
     "core/simple_cxx" => :(include(joinpath(@__DIR__, "core", "simple_cxx.jl"))),
