03411e2b0fb9c59e6ec5d8e93c8633a6fcdb036c
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-04-21 17:10:44 +0900

    Clarify missing exceptions lane failure

--- test/testsuite_config.jl
+++ test/testsuite_config.jl
@@ -20,9 +20,17 @@ function extended_testsuite()
     )
 end
 
+function suite_entry(path::AbstractString, missing_message::AbstractString)
+    isfile(path) ? :(include($path)) : :(throw(ErrorException($missing_message)))
+end
+
 function exception_testsuite()
+    path = joinpath(@__DIR__, "optin", "exceptions_basic.jl")
     Dict(
-        "optin/exceptions_basic" => :(include(joinpath(@__DIR__, "optin", "exceptions_basic.jl"))),
+        "optin/exceptions_basic" => suite_entry(
+            path,
+            "test/optin/exceptions_basic.jl is not present yet; enable the exceptions lane only after that test file lands",
+        ),
     )
 end
 
