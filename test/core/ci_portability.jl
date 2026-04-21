4059d4492385a6cf476774da5986e13ac0bdb8d4
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-04-21 09:10:41 +0900

    ci: inspect windows crt import libraries

--- test/core/ci_portability.jl
+++ test/core/ci_portability.jl
@@ -5,4 +5,6 @@ workflow = read(joinpath(@__DIR__, "..", "..", ".github", "workflows", "ci.yml")
 @testset "ci_portability" begin
     @test occursin("name: Toolchain info", workflow)
     @test occursin("WINDOWS_CLANG_VERBOSE: \"1\"", workflow)
+    @test occursin("command -v llvm-nm.exe || true", workflow)
+    @test occursin("libmsvcrt.a", workflow)
 end
